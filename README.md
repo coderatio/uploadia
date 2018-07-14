185fb63f7ec5db9692548ef594d52d31f8a3d3e2
Josiah Ovye Yahaya <36877085+coderatio@users.noreply.github.com>
2018-07-14 17:41:17 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# uploadia
+# Uploadia
 A flexible PHP file upload library
+
+# Download
+``composer require coderatio/uploadia`` 
+
+Or Clone this repo.
+
+# Usage
+```php
+require 'vendor/autoload.php'; // if you downloaded via composer
+// OR
+require 'pathtouploadia/src/Uploadia.php';
+
+$uploadia = new Coderatio\Uploadia\Uploadia();
+$uploadia->setDir('uploads')
+    ->setExtensions(['jpg', 'png', 'jpeg', 'gif'])
+    ->setMaxSize(1); // In megabytes. 1MB
+    
+// There are several methods to use. e.g $uploadia->allowAllFormats(), $uploadia->sameName($bool) takes a boolean. e.t.c
+    
+if($uploadia->uploadFile('photo') {
+     // photo is the file input name.
+     // Get the uploaded file name or get error message.
+     
+     $uploadia->getUploadName(); // Returns uploaded file name;
+     $uploadia->getMessage(); // Returns error messages
+}
+```
+
+# Contribution
+Fork the project and send pull request, email me via josiahoyahaya@gmail.com, find me on <a href="https://twitter.com/josiahoyahaya">Twitter</a> and say something about it anywhere possible.
+
+# Licence
+This project is licenced under MIT license.
--- src/Uploadia.php
+++ src/Uploadia.php
@@ -21,102 +21,124 @@ class Uploadia
     public $sequence = '';
     public $name = 'Uploader';
     public $sameName = false;
-    public $useTable    = false;
+    public $useTable = false;
 
-    function setDir($path){
-        $this->destinationPath  =   $path;
-        $this->allowAll =   false;
-
-        return $this;
-    }
-
-    function allowAllFormats(){
-        $this->allowAll =   true;
+    function setDir($path)
+    {
+        $this->destinationPath = $path;
+        $this->allowAll = false;
 
         return $this;
     }
 
-    function setMaxSize($sizeMB){
-        $this->maxSize  =   $sizeMB * (1024*1024);
+    function allowAllFormats()
+    {
+        $this->allowAll = true;
 
         return $this;
     }
 
-    function setExtensions($options){
-        $this->extensions   =   $options;
+    function setMaxSize($sizeMB)
+    {
+        $this->maxSize = $sizeMB * (1024 * 1024);
 
         return $this;
     }
 
-    function setSameFileName(){
-        $this->sameFileName =   true;
-        $this->sameName =   true;
+    function setExtensions($extensions)
+    {
+        $this->extensions = $extensions;
 
         return $this;
     }
-    function getExtension($string){
-        try{
-            $parts  =   explode(".",$string);
-            $ext        =   strtolower($parts[count($parts)-1]);
-        }catch(Exception $c){
-            $ext    =   "";
+
+    function setSameFileName()
+    {
+        $this->sameFileName = true;
+        $this->sameName = true;
+
+        return $this;
+    }
+
+    function getExtension($string)
+    {
+        try {
+            $parts = explode(".", $string);
+            $ext = strtolower($parts[count($parts) - 1]);
+        } catch (Exception $c) {
+            $ext = "";
         }
+
         return $ext;
     }
 
-    function setMessage($message){
-        $this->errorMessage =   $message;
+    function setMessage($message)
+    {
+        $this->errorMessage = $message;
 
         return $this;
     }
 
-    function getMessage(){
+    function getMessage()
+    {
         return $this->errorMessage;
     }
 
-    function getUploadName(){
+    function getUploadName()
+    {
         return $this->uploadName;
     }
-    function setSequence($seq){
-        $this->sequence =   $seq;
+
+    function setSequence($seq)
+    {
+        $this->sequence = $seq;
 
         return $this;
     }
 
-    function getRandom(){
-        return strtotime(date('Y-m-d H:i:s')).rand(1111,9999).rand(11,99).rand(111,999);
+    function getRandom()
+    {
+        return strtotime(date('Y-m-d H:i:s')) . rand(1111, 9999) . rand(11, 99) . rand(111, 999);
     }
-    function sameName($true){
-        $this->sameName =   $true;
+
+    function sameName($true)
+    {
+        $this->sameName = $true;
 
         return $this;
     }
-    function uploadFile($fileBrowse){
-        $result =   false;
-        $size   =   $_FILES[$fileBrowse]["size"];
-        $name   =   $_FILES[$fileBrowse]["name"];
-        $ext    =   $this->getExtension($name);
-        if(isset($fileBrowse)) {
-            if(!is_dir($this->destinationPath)){
+
+    function uploadFile($fileBrowse)
+    {
+        $result = false;
+        if ($this->selected($fileBrowse)) {
+            $size = $_FILES[$fileBrowse]["size"];
+            $name = $_FILES[$fileBrowse]["name"];
+            $ext = $this->getExtension($name);
+
+            if (!is_dir($this->destinationPath)) {
                 $this->setMessage("Destination folder is not a directory ");
-            }else if(!is_writable($this->destinationPath)){
+            } else if (!is_writable($this->destinationPath)) {
                 $this->setMessage("Destination is not writable !");
-            }else if(empty($name)){
+            } else if (empty($name)) {
                 $this->setMessage("File not selected ");
-            }else if($size>$this->maxSize){
+            } else if ($size > $this->maxSize) {
                 $this->setMessage("Too large file !");
-            }else if($this->allowAll || (!$this->allowAll && in_array($ext,$this->extensions))){
+            } else if ($this->allowAll || (!$this->allowAll && in_array($ext, $this->extensions))) {
 
-                if($this->sameName==false){
-                    $this->uploadName   =  $this->sequence."_".substr(md5(rand(1111,9999)),0,8).$this->getRandom().rand(1111,1000).rand(99,9999).".".$ext;
-                }else{
-                    $this->uploadName=  $name;
+                if ($this->sameName == false) {
+                    $this->uploadName = $this->sequence . "_" . substr(md5(rand(1111, 9999)), 0, 8) . $this->getRandom() . rand(1111, 1000) . rand(99, 9999) . "." . $ext;
+                } else {
+                    $this->uploadName = $name;
                 }
-                if(move_uploaded_file($_FILES[$fileBrowse]["tmp_name"],$this->destinationPath .'/'. $this->uploadName)){
-                    $result =   true;
-                }else{
+
+                if (move_uploaded_file($_FILES[$fileBrowse]["tmp_name"], $this->destinationPath . '/' . $this->uploadName)) {
+                    $result = true;
+                    $this->setMessage("File uploaded");
+                } else {
                     $this->setMessage("Upload failed , try later !");
                 }
+
             } else {
                 $this->setMessage("Invalid file format !");
             }
@@ -126,16 +148,18 @@ class Uploadia
         return $result;
     }
 
-    function selected($file) {
-        if($_FILES[$file]['size'] > 0) {
+    function selected($file)
+    {
+        if ($_FILES[$file]['size'] > 0) {
             return true;
-        }else {
+        } else {
             return false;
         }
     }
 
-    function deleteUploaded(){
-        return @unlink($this->destinationPath.$this->uploadName);
+    function deleteUploaded()
+    {
+        return @unlink($this->destinationPath . $this->uploadName);
     }
 
 }
